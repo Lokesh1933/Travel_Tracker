e9cda515d8443f3fb8783047a079fde31311e7ff
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-05-28 00:52:28 +0530

    change

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ import pg from "pg"
 const app = express();
 const port = 3000;
 const db = new pg.Client({
-  user: process.env.DB_USER,
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: process.env.PASSWORD,
+  port: 5432,
 })
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+db.connect()
+const result = await db.query("SELECT * FROM visited_countries",(res,err) =>{
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    console.log("Query result:", res.rows);
+  }
+})
+  
 app.get("/", async (req, res) => {
   //Write your code here.
+  const result = await db.query("SELECT * FROM visited_countries")
+ res.render("index.ejs",{countries:result.rows, total: result.rows.length});
 
 });
 
