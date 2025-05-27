123f6af8a64c1116ed0fff9dda4a33c8edb5e050
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-05-28 00:16:03 +0530

    minor change

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+import dotenv from "dotenv"
+dotenv.config()
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg"
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: process.env.DB_USER,
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+
 });
 
 app.listen(port, () => {
