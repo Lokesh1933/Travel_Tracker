3a37e40aa4326c5a2dba2e9c14168b16a46266fc
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-05-28 01:11:57 +0530

    useless commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg"
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -12,26 +12,25 @@ const db = new pg.Client({
   database: "world",
   password: process.env.PASSWORD,
   port: 5432,
-})
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-db.connect()
-  
+db.connect();
+
 app.get("/", async (req, res) => {
-  //Write your code here.
-  const result = await db.query("SELECT country_code FROM visited_countries")
+  
+  const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   console.log(result.rows);
 
- res.render("index.ejs",{countries:countries, total: countries.length});
- db.end()
-
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
